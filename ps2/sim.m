1792602cc9b830e274442900e820d542368a0c09
Chris Poliquin <chrispoliquin@gmail.com>
2015-03-19 20:36:58 -0400

    Simulate entry game for F firms in M markets

--- ps2/sim.m
+++ ps2/sim.m
@@ -1,20 +1,24 @@
-function [] = sim(F, M)
+function [mrkts, costs, firms] = sim(F, M)
     % SIM - Simulate market equilibrium of entry model.
-    %
-    %
+    %   Simulate an entry game in M markets with F potential entrants in which
+    %   firms make entry decisions sequentially, ordered by fixed costs.
     % Input arguments:
     %   M = number of markets to simulate
     %   F = number of potential entrants to simulate
     % Outputs:
-    %
-    %
+    %   mrkts = M by F+2 matrix with columns for market characteristic,
+    %           number of entrants, and profits of each firm
+    %   costs = M by F matrix with fixed cost of each firm in columns
+    %   firms = M by F matrix with firm-market characteristics in columns
     
     % model parameters
-    MU = 2;
-    SIGMA = 1;
-    ALPHA = 1;
-    BETA = 1;
-    DELTA = 1;
+    MU = 2; SIGMA = 1;
+    ALPHA = 1; BETA = 1; DELTA = 1;
+
+    % allocate space for outcome in each market (simulated below)
+    mrkts = zeros(M, F + 2);  % market characteristic, #entrants, and profits
+    costs = zeros(M, F);      % fixed costs
+    firms = zeros(M, F);      % firm-market characteristics
 
     for m = 1:M  % simulate each market individually
         
@@ -23,9 +27,25 @@ function [] = sim(F, M)
         
         % fixed costs for each potential entrant
         phi = ALPHA * Zf + normrnd(MU, SIGMA, F, 1);
+        
+        % order firm variables by fixed costs
+        [~, idx] = sortrows(phi);
+        phi = phi(idx);
+        Zf = Zf(idx);
 
-        profit = @(n) BETA * Xm - DELTA * log(n) - phi;
+        % profits that would result from sequential entry of each firm
+        profits = @(n) BETA * Xm - DELTA * log(n) - phi;
+        sequential_profits = profits([1:3]');
+
+        % which firm is the last firm able to earn a non-negative profit?
+        entrants = sum(sequential_profits >= 0);
+        % what are profits for all firms when this many firms enter?
+        realized_profits = profits(entrants);
+        realized_profits(entrants + 1:end) = 0;  % non-entrants earn zero
     
+        % save this market data for estimation problems
+        mrkts(m,:) = [Xm, entrants, realized_profits'];
+        costs(m,:) = phi';
+        firms(m,:) = Zf';
     end
-
 end
