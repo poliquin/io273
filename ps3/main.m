8001df57e8b9623f008c6f26f9ad82fc0c29712d
Chris Poliquin <chrispoliquin@gmail.com>
2015-04-16 22:13:57 -0400

    Estimate cdf of values for ascending data auction

--- ps3/main.m
+++ ps3/main.m
@@ -7,3 +7,10 @@ rng(8675309);
 dir = mfilename('fullpath');
 cd(dir(1:end-4));
 
+%% Section 1, Question 1
+% ----------------------------------------------------------------------------
+
+X = dlmread('ascending_data.dat');
+F = orderstat(X, 34, 100, 2);
+plot(34:2:100, mean(F, 2))
+
---
+++ ps3/orderstat.m
@@ -0,0 +1,29 @@
+function [F] = orderstat(X, small, large, stepsize)
+    % Estimate CDF of private values, F(v) from ascending auction data
+    % using distribution for n-1 order statistic, G(v).
+
+    syms x;  % distribution of second order statistic with n bidders
+    ostat = @(n, g, x) (n - (n-1)*x) * x^(n-1) - g;  % g = G(v)
+    
+    unq = unique(X(:,1));  % number of bidders that participate in auctions
+    nu = size(unq, 1);
+    grp = arrayfun(@(i) X(X(:,1)==i, 2), unq, 'UniformOutput', false);
+    
+    steps = ceil((large - small) / stepsize);
+    F = zeros(steps, nu);
+    
+    i = 0;
+    for v = small:stepsize:large
+        i = i + 1;
+        % get cdf of n-1 order statistic at value v for auctions with N bidders
+        g = cellfun(@(bids) mean(bids < v), grp);
+
+        % find roots for n-1 order statistic, which equal F(v)
+        % this function solves expression for n-1 order statistic for F(v),
+        % given a number of bidders n and a value for G(v).
+        f = arrayfun(@(t) fzero(@(x) ostat(unq(t), g(t), x), 0.5), 1:nu);
+        F(i,:) = f;
+    end
+
+    F = F(1:i,:);
+end
