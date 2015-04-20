f0d55fb61b03d9eded74c39f335f8514197da8b1
Chris Poliquin <chrispoliquin@gmail.com>
2015-04-19 23:49:18 -0400

    Estimate values in first price auction

--- ps3/main.m
+++ ps3/main.m
@@ -43,3 +43,37 @@ ylabel('')
 title('Bounds on CDF of Value Distribution', 'FontSize', 14)
 saveas(f, 'figs/ascending_bounds.png');
 
+%% Section 1, Question 3 (asymmetric private values, first price auction)
+% ----------------------------------------------------------------------------
+
+fpa = dlmread('fpa.dat');
+[T M] = size(fpa);
+
+FU = zeros(T, M);  % matrix to hold estimated values
+bw = 12;  % bandwidth parameter for kernel function
+% get psuedo private values for each bidder, i
+for i = 1:M
+    sprintf('Estimating values for bidder %i', i)
+    bidi = fpa(:,i);          % player i bids
+    noti = fpa;
+    noti(:,i) = [];           % bidders other than player i
+    maxj = max(noti, [], 2);  % max opponent bid
+    
+    % find value for player i in each auction
+    FU(:,i) = bidshade(bidi, maxj, bw);
+end
+
+% use psuedo private values to estimate empirical cdf for each bidder
+ui = mat2cell(FU, T, ones(1, M));
+cdf = arrayfun(@(z) cell2mat(cellfun(@(x) mean(x < z), ui, 'UniformOutput', ...
+                             false)), [0:2:160]', 'UniformOutput', false);
+cdf = cell2mat(cdf);
+
+% plot value distribution for each bidder
+f = figure('PaperPosition', [.1, .2, 6.2, 3.5], 'PaperSize', [6.4, 4]);
+p1 = plot(0:2:160, cdf);
+xlabel('Value')
+ylabel('')
+title('CDF of Value Distributions, 4 Bidders')
+saveas(f, 'figs/fpa_values.pdf');
+
