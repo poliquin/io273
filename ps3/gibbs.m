ba0c93c32a6a75166cf3461690d0ff6fbe7773b5
dkim0718 <dkim0718@gmail.com>
2015-04-22 22:46:19 -0400

    Revised integration region and draw random initial values

--- ps3/gibbs.m
+++ ps3/gibbs.m
@@ -2,13 +2,13 @@
 % These functions estimate the Gibbs sampler for pset 3.
 function [ALPHA, BETA, SIGMA] = gibbs(Y,X,runs,initbeta,initsigma)
     % Returns results from Gibbs Multinomial Probit
-    %
     % INPUTS
     %   Y = N by 1 vector
     %   X = N*K by P vector
     %        P denotes the number of parameters 
     %   runs = number of runs
-    %
+    %   initbeta = 1 by K row vector of initial values for [beta, alpha]
+    %   initsigma = initial value for sigma (scalar or K by K)
     % OUTPUTS
     %   ALPHA = runs by 1 vector
     %   BETA = runs by 1 vector
@@ -32,6 +32,8 @@ function [ALPHA, BETA, SIGMA] = gibbs(Y,X,runs,initbeta,initsigma)
     % Set diffuse priors (Based on R Code by Rossi)
     betabar = zeros(K,1); % Prior mean for BETA
     A = 0.01 * eye(K); % Prior variance for BETA
+%     betabar = [2;2];
+%     A=eye(K);
     NU = K+2; % Prior DoF for SIGMA
     V = NU * eye(K); % Prior scale matrix for SIGMA
     y_init = zeros(K,1);
@@ -65,14 +67,15 @@ end
 function [y_star, y_term] = drawystar(X, Y, beta, sigma,y_init)
     % Draws y_star from a Normal distribution centered at Xbeta with
     % variance sigma
-    %
     % INPUT
     %   X = N*K by p vector of observed data
     %   Y = N by 1 vector of outcomes
     %   beta = p by 1 vector of coefficients
-    %
+    %   sigma = covariane matrix
+    %   y_init = initial value for y_star
     % OUTPUT
     %   y_star = N*K by 1 vector of imputed latent values
+    %   y_term = ending value of y_star; use for next iteration.
     
     % Initial value for ystar is zero
     global K; global N;
@@ -93,44 +96,40 @@ function [y_star, y_term] = drawystar(X, Y, beta, sigma,y_init)
             weight = [1,1;1,-1];
         end
         
-        % Update beta_i
-        for j=1:K-1
-            % Draw from truncated normal
-            cont = true;
-            c_count = 1;
-            while cont
-                ystar_new = ystar_old;
-                % Calculate posterior conditional means
-                z_i = X((i-1)*K+1:i*K,:); % [x_i, p_i]=2by2 matrix
-%                 [mu, tau] = postmean(z_i, beta, sigma,ystar_new,j);
-                mu = z_i * beta;
-                tau = sigma;
-                
-                % Draw from posterior using simple acceptance algorithm
-%                 ystar_new(j) = normrnd(mu,tau);
-                ystar_new = mvnrnd(mu,tau)';
-                Ay = weight * ystar_new;
-                if y == 1
-                    % Need Ay>0
-                    cont = not(all(Ay >= 0));
-                else
-                    % Need Ay <0
-                    cont = not(all(Ay <= 0));
-                end
-                % Update ystar
-                
-                c_count = c_count+1;
-                if mod(c_count,100000) == 0
-                    % If it gets stuck, push it a little bit
-                    sigma = sigma * 100;
-                    disp('Got stuck; multiply sigma by 100')
-                    disp(['i, c_count, y, eval'])
-                    disp([i,c_count,y])
-                    disp(weight * ystar_new)
-                end
+    % Update beta_i
+        % Draw from truncated normal
+        cont = true;
+        c_count = 1;
+        while cont
+            ystar_new = ystar_old;
+            % Calculate posterior conditional means
+            z_i = X((i-1)*K+1:i*K,:); % [x_i, p_i]=2by2 matrix
+            mu = z_i * beta;
+            tau = sigma;
+
+            % Draw from posterior using simple acceptance algorithm
+            ystar_new = mvnrnd(mu,tau)';
+            Ay = weight * ystar_new;
+            Ay(Ay<0)=0;
+            Ay=all(Ay);
+            if y == Ay
+                cont = false;
+            else
+                cont = true;
             end
-            ystar_old = ystar_new;
+            % Update ystar
+
+            c_count = c_count+1;
+            if mod(c_count,100000) == 0
+                % If it gets stuck, push it a little bit
+                sigma = sigma * 100;
+                disp('Got stuck; multiply sigma by 100')
+                disp(['i, c_count, y, eval'])
+                disp([i,c_count,y])
+                disp(weight * ystar_new)
+            end
         end
+        ystar_old = ystar_new;
         y_star((i-1)*K+1:i*K,:) = ystar_new;
     end
     y_term = ystar_new;
@@ -139,8 +138,12 @@ end
 function beta = drawbeta(y_star, X, betabar, sigma, A)
     % Draws beta from posterior Normal distribution centered at 
     % INPUTS
-    %   y_star = Latent variables that 
+    %   y_star = Latent variables
     %   sigma = K by K covariance matrix
+    %   X = [X, P] in pset 3
+    %   betabar = prior mean on beta
+    %   sigma = prior on sigma
+    %   A = prior variance on beta
     % OUTPUTS
     %   beta = K by 1 posterior draw of beta
     % Transform variables by multipyling by C and stack
@@ -166,49 +169,47 @@ function sigma = drawsigma(X,b,ystar,V,NU)
     global N;
     % Run regression, get epsilons
     epsilon = reshape(ystar - X*b,2,[]);
-%     S = epsilon*epsilon';
-%     S = diag(sum(epsilon.^2,2));
     
     % Draw from posterior
     sigma = iwishrnd(V + epsilon*epsilon',NU+N);
 end
 
-function [mu, tau] = postmean(X_i,beta,sigma,w_i,j)
-    % Returns posterior mean (2by1) and variance (2by2) from the given data
-    % INPUTS
-    %   X_i = 2 by 2 matrix of data ([x_i, p_i] in pset)
-    %   beta = 2 by 1 vector of coefficients ([BETA, ALPHA] in pset)
-    %   sigma = 2 by 2 covariance matrix
-    %   w_i = 2 by 1 vector
-    %   j = Either 1 or 2.
-    % OUTPUTS
-    %   mu = p by 1 vector of means
-    %   sig = p by p matrix of covariances
-    global K;
-%     % following book
-%     siginv = sigma\eye(K);
-%     gamma = siginv(j,:);
-%     gamma(j) = [];
-%     F = -siginv(j,j) * gamma;
-%     xij = X_i(j,:)';
-%     xi_j = X_i;
-%     xi_j(j,:)=[]; % delete jth row
-%     wi_j = w_i;
-%     wi_j(j) = [];
-%     
-%     % Check for empty matrices
-%     if isempty(F)
-%         F=0;
-%     end
-%     if isempty(xi_j)
-%         xi_j=0;
-%     end
-%     if isempty(wi_j)
-%         wi_j=0;
-%     end
-%     mu = xij'*beta + F'*(wi_j-xi_j*beta);
-%     tau = 1/siginv(j,j);
-    % Simply drawing from multivariate normal
-    mu = X_i * beta;
-    tau = sigma;
-end
+% function [mu, tau] = postmean(X_i,beta,sigma,w_i,j)
+%     % Returns posterior mean (2by1) and variance (2by2) from the given data
+%     % INPUTS
+%     %   X_i = 2 by 2 matrix of data ([x_i, p_i] in pset)
+%     %   beta = 2 by 1 vector of coefficients ([BETA, ALPHA] in pset)
+%     %   sigma = 2 by 2 covariance matrix
+%     %   w_i = 2 by 1 vector
+%     %   j = Either 1 or 2.
+%     % OUTPUTS
+%     %   mu = p by 1 vector of means
+%     %   sig = p by p matrix of covariances
+%     global K;
+% %     % following book
+% %     siginv = sigma\eye(K);
+% %     gamma = siginv(j,:);
+% %     gamma(j) = [];
+% %     F = -siginv(j,j) * gamma;
+% %     xij = X_i(j,:)';
+% %     xi_j = X_i;
+% %     xi_j(j,:)=[]; % delete jth row
+% %     wi_j = w_i;
+% %     wi_j(j) = [];
+% %     
+% %     % Check for empty matrices
+% %     if isempty(F)
+% %         F=0;
+% %     end
+% %     if isempty(xi_j)
+% %         xi_j=0;
+% %     end
+% %     if isempty(wi_j)
+% %         wi_j=0;
+% %     end
+% %     mu = xij'*beta + F'*(wi_j-xi_j*beta);
+% %     tau = 1/siginv(j,j);
+%     % Simply drawing from multivariate normal
+%     mu = X_i * beta;
+%     tau = sigma;
+% end
--- ps3/sec2.m
+++ ps3/sec2.m
@@ -6,7 +6,27 @@ cd(dir(1:end-4));
 
 %% Simulate dataset
 N=100;  K=2; SIGMA=[1,1;1,2]; ALPHA=1; BETA=1;
-[Y, X, P,A] = sim_dataset(N, K, SIGMA, ALPHA, BETA);
+[Y, X, P,As] = sim_dataset(N, K, SIGMA, ALPHA, BETA);
 
-%% Run gibbs
-[A,B,S] = gibbs(Y,[X,P],5000,[-10,-10],100);
+%% Run gibbs using three different starting points and 10000 runs
+runs = 10000;
+init = zeros(3,2); initsig = zeros(2,2,3);
+alpha = zeros(runs+1,3); beta = zeros(runs+1,3);
+sigma = zeros(2,2,runs+1,3);
+for i = 1:1
+    init(i,:) = mvnrnd([1,1],100*eye(2)); initsig(:,:,i)= wishrnd(eye(2),30);
+    [A,B,S] = gibbs(Y,[X,P],runs,init(i,:),initsig(:,:,i));
+    scale = squeeze(S(1,1,:));
+    beta(:,i) = B./scale;
+    alpha(:,i) = A./scale;
+    sigma(:,:,:,i) = S./repmat(S(1,1,:),2);
+end
+
+%% Graphs
+% beta vs alpha
+scatter(A(500:end),B(500:end))
+
+% Sigma
+plot(squeeze(S(1,1,500:end)))
+hold on
+plot(squeeze(S(2,2,500:end)))
