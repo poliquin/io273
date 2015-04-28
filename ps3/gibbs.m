69b12565034ba71a159338c8f40e5cabcbb5a01d
dkim0718 <dkim0718@gmail.com>
2015-04-28 00:07:04 -0400

    All working except 2(b)

--- ps3/gibbs.m
+++ ps3/gibbs.m
@@ -1,6 +1,6 @@
 %% Question 2 - Gibbs
 % These functions estimate the Gibbs sampler for pset 3.
-function [ALPHA, BETA, SIGMA] = gibbs(Y,X,runs,initbeta,initsigma)
+function [ALPHA, BETA, SIGMA, ystr] = gibbs(Y,X,runs,initbeta,initsigma)
     % Returns results from Gibbs Multinomial Probit
     % INPUTS
     %   Y = N by 1 vector
@@ -8,104 +8,120 @@ function [ALPHA, BETA, SIGMA] = gibbs(Y,X,runs,initbeta,initsigma)
     %        P denotes the number of parameters 
     %   runs = number of runs
     %   initbeta = 1 by K row vector of initial values for [beta, alpha]
-    %   initsigma = initial value for sigma (scalar or K by K)
+    %   initsigma = initial value for sigma 
     % OUTPUTS
     %   ALPHA = runs by 1 vector
     %   BETA = runs by 1 vector
     %   SIGMA = 2*runs by 2 matrix
-    global K; K = size(X,2); % K is the number of choice options
+    global sigsize; sigsize = length(initsigma);
+    global K; K=2;% K is the number of choice options
     global N; N = 100; 
     
-    % Declare outputs
+    % Allocate space for outputs
     ALPHA=zeros(runs,1); BETA=zeros(runs,1);
-    SIGMA=zeros(2,2,runs); 
+    SIGMA=zeros(sigsize,sigsize,runs); 
+    ystr=zeros(N,sigsize,runs);
     
     % Initialize parameters
     ALPHA(1)=initbeta(2); BETA(1)=initbeta(1);
-    SIGMA(:,:,1)=eye(K)*initsigma; sigmaold = SIGMA(:,:,1);
-    if K==2
-        betaold = [BETA(1); ALPHA(1)];
-    else
-        betaold = BETA(1);
-    end
-    
+    SIGMA(:,:,1)=initsigma; sigmaold = SIGMA(:,:,1);
+    betaold = [BETA(1); ALPHA(1)];
+   
     % Set diffuse priors (Based on R Code by Rossi)
     betabar = zeros(K,1); % Prior mean for BETA
-    A = 0.01 * eye(K); % Prior variance for BETA
-%     betabar = [2;2];
-%     A=eye(K);
-    NU = K+2; % Prior DoF for SIGMA
-    V = NU * eye(K); % Prior scale matrix for SIGMA
-    y_init = zeros(K,1);
+    A = 0.01 * eye(K); % Prior inverse variance for BETA
+    NU = sigsize+2; % Prior DoF for SIGMA
+    V = NU * eye(sigsize); % Prior scale matrix for SIGMA
+    
+    P=reshape(Y(:,2:end)',[],1);
+    Z=X(:,2);
     
     % Run Gibbs procedure
     for i = 1:runs
-        % 1) Draw y_star from a posterior Normal
-        % 2) Draw beta from a posterior Normal
+        % 1) Draw y_star (Nx2) from a posterior Normal
+        % 2) Draw beta (2x1) from a posterior Normal
         % 3) Draw sigma from an inverse Wishart
-        [y_star,y_term] = drawystar(X,Y,betaold,sigmaold,y_init);
+%         if mod(i,100)==0
+%             fprintf('i %i\n',i)
+%             fprintf('beta %3.3f, gamma %3.3f\n',betaold(1),betaold(2))
+%             disp(sigmaold)
+%         end
+        [y_star] = drawystar(X,Y,betaold,sigmaold);
+%         ystr(:,:,i)=y_star;
         betanew = drawbeta(y_star, X, betabar, sigmaold,A);
-        sigmanew = drawsigma(X, betanew, y_star, V, NU);
+        sigmanew = drawsigma(Y,X, betanew, y_star, V, NU);
         
         % Save results
         ALPHA(i+1) = betanew(2);
         BETA(i+1) = betanew(1);
         SIGMA(:,:,i+1) = sigmanew;
         
-        % Update 
+        % Update y*, beta, sigma
         betaold=betanew; sigmaold=sigmanew;
-        y_init = y_term;
         
-        % Print every 100 iterations
-        if mod(i,100)==0
-            disp('i = ')
-            disp(i)
+        % Print every 1000 iterations
+        if mod(i,1000)==0
+            fprintf('Iteration i = %i\n',i)
         end
     end
 end
 
-function [y_star, y_term] = drawystar(X, Y, beta, sigma,y_init)
+function [y_star] = drawystar(X, Y, beta, sigma)
     % Draws y_star from a Normal distribution centered at Xbeta with
     % variance sigma
     % INPUT
     %   X = N*K by p vector of observed data
-    %   Y = N by 1 vector of outcomes
-    %   beta = p by 1 vector of coefficients
+    %   Y = N by K vector of outcomes
+    %   beta = p by 1 vector of coefficients. Always beta(1) = beta with 
+    %           beta(2) = alpha or gamma.
     %   sigma = covariane matrix
     %   y_init = initial value for y_star
     % OUTPUT
-    %   y_star = N*K by 1 vector of imputed latent values
+    %   y_star = N by K vector of imputed latent values (y1*, y2*)
     %   y_term = ending value of y_star; use for next iteration.
     
     % Initial value for ystar is zero
-    global K; global N;
-    N = length(Y);
+    global sigsize; global N; global K;
     
     % Declare variables 
-    y_star = zeros(N*K,1); % y_star stores N values of y_star_i
-    ystar_old = y_init; % ystar_old is old value of y_star_i; to be replaced by y_star_new
+    y_star = zeros(N*2,1); % y_star stores N values of y_star_i
+    if sigsize == 4
+        P=reshape(Y(:,2:end)',[],1);
+        % Need to draw eta
+        eta = P-beta(2)*X(:,2);
+%         tempgam = (X(:,2)'*X(:,2))\(X(:,2)'*P);
+%         eta= P - tempgam*X(:,2); % eta = P-Zgamma (2N by 1)
+        sig11 = sigma(1:2,1:2); sig22 = sigma(3:4,3:4);
+        sig12 = sigma(1:2,3:4); sig21 = sigma(3:4,1:2);
+        sigma = sig11-sig12*(sig22\sig21);
+    end
     
     % For each observation, draw over the posterior
-    for i=1:N
+    for ind=1:N
         % Initialize some parameters
-        y = Y(i);
-        % Constraint matrix (matrix A in pset)
-        if mod(i,2)==0
-            weight = [-1,1;1,1];
-        else
-            weight = [1,1;1,-1];
-        end
+        y = Y(ind);
+        weight = cmat(ind);
         
-    % Update beta_i
-        % Draw from truncated normal
+        % Update beta_i
         cont = true;
         c_count = 1;
         while cont
             % Calculate posterior conditional means
-            z_i = X((i-1)*K+1:i*K,:); % [x_i, p_i]=2by2 matrix
-            mu = z_i * beta;
-            tau = sigma;
-
+            x_i = X((ind-1)*K+1:ind*K,:);
+            if sigsize==4
+                % need to draw from y* ~ N(Xbeta + P + condmean(epsilon),
+                % condvar(epsilon))
+                mu = x_i(:,1)*beta(1) + P((ind-1)*K+1:ind*K,:)+ ... 
+                    sig12*(sig22\eta((ind-1)*K+1:ind*K));
+                tau = sigma;
+%                 T = [1,0,1,0;0,1,0,1];
+%                 mu = x_i*beta;
+%                 tau = T*sigma*T';
+            else
+                mu=x_i*beta;
+                tau = sigma;
+            end
+            
             % Draw from posterior using simple acceptance algorithm
             ystar_new = mvnrnd(mu,tau)';
             Ay = weight * ystar_new;
@@ -116,7 +132,6 @@ function [y_star, y_term] = drawystar(X, Y, beta, sigma,y_init)
             else
                 cont = true;
             end
-            % Update ystar
 
             c_count = c_count+1;
             if mod(c_count,100000) == 0
@@ -124,20 +139,20 @@ function [y_star, y_term] = drawystar(X, Y, beta, sigma,y_init)
                 sigma = sigma * 100;
                 disp('Got stuck; multiply sigma by 100')
                 disp(['i, c_count, y, eval'])
-                disp([i,c_count,y])
+                disp([ind,c_count,y])
                 disp(weight * ystar_new)
+                disp(sigma)
             end
         end
-        ystar_old = ystar_new;
-        y_star((i-1)*K+1:i*K,:) = ystar_new;
+        % Update ystar
+        y_star((ind-1)*K+1:ind*K,:) = ystar_new;
     end
-    y_term = ystar_new;
 end
 
 function beta = drawbeta(y_star, X, betabar, sigma, A)
     % Draws beta from posterior Normal distribution centered at 
     % INPUTS
-    %   y_star = Latent variables
+    %   y_star = N*K by 1 matrix of Latent variables
     %   sigma = K by K covariance matrix
     %   X = [X, P] in pset 3
     %   betabar = prior mean on beta
@@ -146,70 +161,100 @@ function beta = drawbeta(y_star, X, betabar, sigma, A)
     % OUTPUTS
     %   beta = K by 1 posterior draw of beta
     % Transform variables by multipyling by C and stack
-    global K; global N;
-    G = sigma\eye(K); % G is sigma inverted
+    global K; global N; global sigsize;
+    if sigsize ==4
+        % Change variance matrix to correct dimensions.
+        % Is this right??
+        T=[1,0,1,0;0,1,0,1];
+        sigma = T*sigma*T';
+%         % Or is this right??
+%         sig11 = sigma(1:2,1:2); sig22=sigma(3:4,3:4);
+%         sig12 = sigma(1:2,3:4); sig21 = sigma(3:4,1:2);
+%         sigma = sig11-sig12*(sig22\sig21);
+%         y_star=y_star-etahat;
+    end
+    G = sigma\eye(2); % G is sigma inverted
     C = chol(G,'lower');
-    %C = sqrtm(G);
-    Xstar = kron(eye(N),C')*X;
-    ystar = kron(eye(N),C')*y_star;
-    % Calculate mean and variance (p.213 McCulloch & Rossi 1994)
+    Xstar = kron(eye(N),C)*X;
+    ystar = kron(eye(N),C)*y_star;
     sig = (Xstar'*Xstar + A)\eye(K);
-    betahat = sig * (Xstar'*ystar + A*betabar);
+    betahat = sig * (Xstar'*ystar+A*betabar);
     
     % Draw random normal
     beta = mvnrnd(betahat,sig)';
 end
 
-function sigma = drawsigma(X,b,ystar,V,NU)
+function sigma = drawsigma(Y,X,b,ystar,V,NU)
     % Draw sigma from inverted Wishart distribution
     % INPUTS
-    %   X = N*K by 2 matrix
+    %   Y = [Y, P1, P2] matrix (N by 3)
+    %   X = N*K by 2 matrix of either [X,P] or [X,Z]
     %   b = 2 by 1 vector (=[BETA,ALPHA])
-    %   ystar = N*K by 1 vector of latent utilities
-    global N; global K;
+    %   ystar = N by K vector of latent utilities
+    %   NU = scalar addition to dof in IW distribution
+    % OUTPUTS
+    %   sigma = sigsize * sigsize covariance matrix
+    global N; global sigsize;
     % Run regression, get epsilons
-    epsilon = reshape(ystar - X*b,2,[]);
-    
+    if sigsize == 2
+        error=reshape(ystar-X*b,2,[])'; %N by 2
+    elseif sigsize == 4
+        Z = X(:,2);
+        X = X(:,1);
+        P = reshape(Y(:,2:3)',[],1);
+        beta = b(1);
+        gamma = b(2);
+        etahat=P-Z*gamma; % 2N by 1
+        epsilon=ystar-X*beta-P; % 2N by 1
+%         etahat = draweta(P, Z, gamma, epsilon, omega);
+        % Reshape
+        eta = reshape(etahat,2,[])';
+        epsilon=reshape(epsilon,2,[])';
+        error = [epsilon,eta]; % N by 4
+    end
     % Draw from posterior
-    sigma = iwishrnd(V + epsilon*epsilon',NU+N);
+    sigma = iwishrnd(V + error'*error,NU+N);
 end
-
-% function [mu, tau] = postmean(X_i,beta,sigma,w_i,j)
-%     % Returns posterior mean (2by1) and variance (2by2) from the given data
+% 
+% function etahat = draweta(P, Z, gam, eps, sigma)
+%     % Draw eta by Gibbs'ing through the observations from posterior 
+%     % conditional posterior normal centered at P-Z*gamma, given epsilon
 %     % INPUTS
-%     %   X_i = 2 by 2 matrix of data ([x_i, p_i] in pset)
-%     %   beta = 2 by 1 vector of coefficients ([BETA, ALPHA] in pset)
-%     %   sigma = 2 by 2 covariance matrix
-%     %   w_i = 2 by 1 vector
-%     %   j = Either 1 or 2.
+%     %   P = N*K vector 
+%     %   Z = N*K vector
+%     %   gam = scalar parameter
+%     %   epsilon = N*K vector of imputed errors
+%     %   Omega = sigsize*sigsize matrix of covariances
 %     % OUTPUTS
-%     %   mu = p by 1 vector of means
-%     %   sig = p by p matrix of covariances
-%     global K;
-% %     % following book
-% %     siginv = sigma\eye(K);
-% %     gamma = siginv(j,:);
-% %     gamma(j) = [];
-% %     F = -siginv(j,j) * gamma;
-% %     xij = X_i(j,:)';
-% %     xi_j = X_i;
-% %     xi_j(j,:)=[]; % delete jth row
-% %     wi_j = w_i;
-% %     wi_j(j) = [];
-% %     
-% %     % Check for empty matrices
-% %     if isempty(F)
-% %         F=0;
-% %     end
-% %     if isempty(xi_j)
-% %         xi_j=0;
-% %     end
-% %     if isempty(wi_j)
-% %         wi_j=0;
-% %     end
-% %     mu = xij'*beta + F'*(wi_j-xi_j*beta);
-% %     tau = 1/siginv(j,j);
-%     % Simply drawing from multivariate normal
-%     mu = X_i * beta;
-%     tau = sigma;
+%     %   etahat = N*K vector of draw from conditional posterior distribution
+%     global N; global K;
+%     etahat = zeros(N*K,1);
+%     % Get covariance matrix and mean conditional on epsilon
+%     sig11 = sigma(1:2,1:2); sig22=sigma(3:4,3:4);
+%     sig12 = sigma(1:2,3:4); sig21 = sigma(3:4,1:2);
+%     postsig = sig22 - sig21 * (sig11\sig12);
+%     % This is the mean? Maybe this should be zero
+%     tempgam = (Z'*Z)\(Z'*P);
+%     eta = P - Z*tempgam;
+% %     etahat_i = zeros(K,1); % Initial value for etahat
+%     for ind = 1:N
+%         % Init variables 
+%         epsilon_i = eps((ind-1)*K+1:ind*K);
+%         etaold = eta((ind-1)*K+1:ind*K);
+% %         etaold = etahat_i; %update old value
+%         % Get posterior mean centered on old value
+% %         postmu = etaold + sig21 * (sig11\epsilon_i);
+%         postmu = sig21 * (sig11\epsilon_i);
+%         % Draw from posterior and save
+%         etahat_i = mvnrnd(postmu,postsig)';
+%         etahat((ind-1)*K+1:ind*K)=etahat_i;
+%     end
 % end
+
+function weight = cmat(i)
+    if mod(i,2)==0
+        weight = [-1,1;1,1];
+    else
+        weight = [1,1;1,-1];
+    end
+end
--- ps3/sec2.m
+++ ps3/sec2.m
@@ -1,5 +1,5 @@
 clear all
-
+%%
 rng(8675309);
 dir = mfilename('fullpath');
 cd(dir(1:end-4));
@@ -7,18 +7,163 @@ cd(dir(1:end-4));
 %% Simulate dataset
 N=100;  K=2; SIGMA=[1,1;1,2]; ALPHA=1; BETA=1;
 [Y, X, P,As] = sim_dataset(N, K, SIGMA, ALPHA, BETA);
+disp('Mean and std. dev. for simulated data')
+disp(table(num2str([mean(Y);std(Y)], '%3.3f &'), ...
+    num2str([mean(X(:,1));std(X(:,1))], '%3.3f &'), ...
+    num2str([mean(X(:,2));std(X(:,2))], '%3.3f &'), ...
+            num2str([mean(P(:,1));std(P(:,1))], '%3.3f &'), ...
+            num2str([mean(P(:,2));std(P(:,2))], '%3.3f \\\\'), ...
+            'VariableNames', {'Y' 'X1' 'X2' 'P1' 'P2'}, ...
+            'RowNames', {'Mean &' 'Std. Dev &'}))
+disp(' ')
 
+% Reshape X, P
+X = reshape(X',[],1);
+P = reshape(P',[],1);
 %% Run gibbs using three different starting points and 10000 runs
-runs = 3000; inits = 1;
+runs = 10000; inits = 3;
+init = zeros(inits,2); initsig = zeros(2,2,inits);
 alpha = zeros(runs+1,inits); beta = zeros(runs+1,inits);
 sigma = zeros(2,2,runs+1,inits);
-for i = 1:inits
-    init(i,:) = [1,1]; initsig(:,:,i)= [1,1;1,2];
-    [A,B,S] = gibbs(Y,[X,P],runs,init(i,:),initsig(:,:,i));
-    scale = squeeze(S(1,1,:));
-end
-    beta = B./sqrt(scale);
-    alpha = A./sqrt(scale);
-    sigma = S./repmat(S(1,1,:),2);
 
+% MCMC iterations
+for ind = 1:inits
+    init(ind,:) = mvnrnd([1,1],100*eye(2)); initsig(:,:,ind)= wishrnd(10*eye(2),100);
+    printsig = initsig(:,:,ind)/initsig(1,1,ind);
+    fprintf('Initial values: [beta, alpha, sig12, sig21, sig22] = \n %3.3f & %3.3f & %3.3f & %3.3f & %3.3f \\\\ \n',init(ind,1),init(ind,2),printsig(1,2),printsig(2,1),printsig(2,2))
+    [A,B,S,YSTR] = gibbs(Y,[X,P],runs,init(ind,:),initsig(:,:,ind));
+    scale = squeeze(S(1,1,:));
+    beta(:,ind) = B./sqrt(scale);
+    alpha(:,ind) = A./sqrt(scale);
+    sigma(:,:,:,ind) = S./repmat(S(1,1,:),2);
+end
+%     beta = B./sqrt(scale);
+%     alpha = A./sqrt(scale);
+%     sigma = S./repmat(S(1,1,:),2);
+
+% get confidence intervals
+cut = (runs-1000)*0.025;
+a_min=zeros(inits,1); a_max=zeros(inits,1); b_min=zeros(inits,1); b_max=zeros(inits,1);
+s12_min=zeros(inits,1);s21_min=zeros(inits,1);s22_min=zeros(inits,1);
+s12_max=zeros(inits,1);s21_max=zeros(inits,1);s22_max=zeros(inits,1);
+for ind = 1:inits
+    alpha_s = sort(alpha(1000:end,ind));
+    a_min(ind) = min(alpha_s(cut:end-cut));
+    a_max(ind) = max(alpha_s(cut:end-cut));
+    beta_s = sort(beta(1000:end,ind));
+    b_min(ind) = min(beta_s(cut:end-cut));
+    b_max(ind) = max(beta_s(cut:end-cut));
+    s12 = sort(sigma(1,2,1000:end,ind));
+    s21 = sort(sigma(2,1,1000:end,ind));
+    s22 = sort(sigma(2,2,1000:end,ind));
+    s12_min(ind) = min(squeeze(s12(cut:end-cut)));
+    s21_min(ind) = min(squeeze(s21(cut:end-cut)));
+    s22_min(ind) = min(squeeze(s22(cut:end-cut)));
+    s12_max(ind) = max(squeeze(s12(cut:end-cut)));
+    s21_max(ind) = max(squeeze(s21(cut:end-cut)));
+    s22_max(ind) = max(squeeze(s22(cut:end-cut)));
+end
+
+% display confidence intervals
+for ind = 1:inits
+    disp(fprintf('Mean and 95%% confidence interval for simulated coefficents, %i of %i',ind,inits))
+    disp(table(...
+        num2str([mean(alpha(1000:end,ind));a_min(ind);a_max(ind)], '%3.3f &'), ...
+        num2str([mean(beta(1000:end,ind));b_min(ind);b_max(ind)], '%3.3f &'), ...
+        num2str([mean(sigma(1,2,1000:end,ind));s12_min(ind);s12_max(ind)], '%3.3f &'), ...
+        num2str([mean(sigma(2,2,1000:end,ind));s22_min(ind);s22_max(ind)], '%3.3f \\\\'), ...
+        'VariableNames', {'Alpha' 'Beta' 's12' 's22'}, ...
+        'RowNames', {'Mean &' 'lower 95%% &' 'upper 95%%'}))
+    disp(' ')
+end
 %% Graphs
+
+
+%% Part 2
+clear all
+[y2, x2, p2, z2] = sim_dataset2(100,2,1,1);
+disp('Mean and std. dev. for simulated data (Part 2)')
+disp(table(num2str([mean(y2);std(y2)], '%3.3f &'), ...
+    num2str([mean(x2(:,1));std(x2(:,1))], '%3.3f &'), ...
+    num2str([mean(x2(:,2));std(x2(:,2))], '%3.3f &'), ...
+    num2str([mean(p2(:,1));std(p2(:,1))], '%3.3f &'), ...
+    num2str([mean(p2(:,2));std(p2(:,2))], '%3.3f &'), ...
+    num2str([mean(z2(:,1));std(z2(:,1))], '%3.3f &'), ...
+    num2str([mean(z2(:,2));std(z2(:,2))], '%3.3f \\\\'), ...
+    'VariableNames', {'Y' 'X1' 'X2' 'P1' 'P2' 'Z1' 'Z2'}, ...
+    'RowNames', {'Mean &' 'Std. Dev &'}))
+disp(' ')
+y2_in = reshape(y2',[],1);
+x2_in = reshape(x2',[],1);
+p2_in = reshape(p2',[],1);
+%% Part 2-1
+inits = 3; runs =10000;
+beta1=zeros(runs+1,inits); alpha1=zeros(runs+1,inits);
+sigma1=zeros(2,2,runs+1,inits);
+for ind = 1:inits
+    init(ind,:) = mvnrnd([1,1],100*eye(2)); initsig(:,:,ind)= wishrnd(10*eye(2),100);
+    printsig = initsig(:,:,ind)/initsig(1,1,ind);
+    fprintf('Initial values: [beta, alpha, sig12, sig21, sig22] = \n %3.3f & %3.3f & %3.3f & %3.3f & %3.3f \\\\ \n',init(ind,1),init(ind,2),printsig(1,2),printsig(2,1),printsig(2,2))
+    [a1,b1,s1,ystr] = gibbs(y2_in,[x2_in,p2_in],runs,init(ind,:),initsig(:,:,ind));
+    scale = squeeze(s1(1,1,:));
+    beta1(:,ind) = b1./sqrt(scale);
+    alpha1(:,ind) = a1./sqrt(scale);
+    sigma1(:,:,:,ind) = s1./repmat(s1(1,1,:),2);
+end
+
+% get confidence intervals
+cut = (runs-1000)*0.025;
+for ind = 1:inits
+    alpha_s = sort(alpha1(1000:end,ind));
+    a_min(ind) = min(alpha_s(cut:end-cut));
+    a_max(ind) = max(alpha_s(cut:end-cut));
+    beta_s = sort(beta1(1000:end,ind));
+    b_min(ind) = min(beta_s(cut:end-cut));
+    b_max(ind) = max(beta_s(cut:end-cut));
+    s12 = sort(sigma1(1,2,:,ind));
+    s21 = sort(sigma1(2,1,:,ind));
+    s22 = sort(sigma1(2,2,:,ind));
+    s12_min(ind) = min(squeeze(s12(cut:end-cut)));
+    s21_min(ind) = min(squeeze(s21(cut:end-cut)));
+    s22_min(ind) = min(squeeze(s22(cut:end-cut)));
+    s12_max(ind) = max(squeeze(s12(cut:end-cut)));
+    s21_max(ind) = max(squeeze(s21(cut:end-cut)));
+    s22_max(ind) = max(squeeze(s22(cut:end-cut)));
+end
+
+% display confidence intervals
+for ind = 1:inits
+    disp(fprintf('Mean and 95%% confidence interval for simulated coefficents, %i of %i',ind,inits))
+    disp(table(...
+        num2str([mean(alpha(1000:end,ind));a_min(ind);a_max(ind)], '%3.3f &'), ...
+        num2str([mean(beta(1000:end,ind));b_min(ind);b_max(ind)], '%3.3f &'), ...
+        num2str([mean(sigma(1,2,1000:end,ind));s12_min(ind);s12_max(ind)], '%3.3f &'), ...
+        num2str([mean(sigma(2,2,1000:end,ind));s22_min(ind);s22_max(ind)], '%3.3f \\\\'), ...
+        'VariableNames', {'Alpha' 'Beta' 's12' 's22'}, ...
+        'RowNames', {'Mean &' 'lower 95%% &' 'upper 95%%'}))
+    disp(' ')
+end
+%% Part 2-2
+% runs = 10000; inits=3;
+% beta2=zeros(runs+1,inits); gamm2=zeros(runs+1,inits);
+% sigma2=zeros(4,4,runs+1,inits);
+% % Get dataset into desired form
+% inputX = [reshape(x2',[],1),reshape(z2',[],1)];
+% inputY = [y2, p2];
+% for ind = 1:inits
+%     initsig = zeros(4,4,inits);
+%     init(ind,:) = mvnrnd([1,1],100*eye(2)); initsig(:,:,ind)= wishrnd(10*eye(4),100);
+%     printsig = initsig(:,:,ind)/initsig(1,1,ind);
+%     fprintf('Initial values: [beta, gamma] =\n %3.3f & %3.3f \\\\ \n',init(ind,1),init(ind,2))
+%     [g2,b2,s2,ystr]=gibbs(inputY,inputX,runs,init(ind,:),initsig(:,:,ind));
+%     scale2 = squeeze(s2(1,1,:));
+%     beta2(:,ind) = b2./sqrt(scale2);
+%     gamm2(:,ind) = g2./sqrt(scale2);
+%     sigma2(:,ind) = s2./repmat(s2(1,1,:),4);
+% end
+% mean(b2(1000:end))
+% mean(g2(1000:end))
+% mean(s2(:,:,1000:end),3)
+
+
+% GAMMA = 
